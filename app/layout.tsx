99be51f5ee1fe36b8e971879471be4c2fb944807
TheRainHermit <miguel.fabra211294@gmail.com>
2025-10-24 11:08:47 -0500

    Commit 3: Proceso de despliegue, añadir cookies.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={inter.className}>
-        <ContextProvider>{children}</ContextProvider>
+        <ContextProvider cookies={null}>{children}</ContextProvider>
       </body>
     </html>
   )
