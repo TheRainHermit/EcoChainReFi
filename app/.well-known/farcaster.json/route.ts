e8203907540e9ec6fafe80cc05cf00f56b6dab9b
TheRainHermit <miguel.fabra211294@gmail.com>
2025-11-03 12:57:44 -0500

    Commit 16: Integración de Base Miniapp 3

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -8,9 +8,9 @@ export async function GET() {
 const URL = process.env.NEXT_PUBLIC_URL as string;
 return Response.json({
   "accountAssociation": {
-    "header": "",
-    "payload": "",
-    "signature": ""
+    "header": "eyJmaWQiOjEzNzAxODMsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhDM0FkMTA0NjIzQ0RhQTI4NTNjZWFFNzlBM2QwQzczOTNERTZhZDEzIn0",
+    "payload": "eyJkb21haW4iOiJlY28tY2hhaW4td2FsbGV0LnZlcmNlbC5hcHAifQ",
+    "signature": "GQc0dGCj/4dN1szwsI9WzvJHWscrW8633qxqHvbWftF4e6V/WCaZtQFwY1RFAYDH+SNc7h5IVHBJZiUWIBhFNhw="
   },
   "baseBuilder": {
     "ownerAddress": "0xaD1559C2A85fed3e749F1CD0C7dd19945F6d5Ea7"
